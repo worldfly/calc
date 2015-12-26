13a54b567ce9a030ecd40fa8574e295ca679a003
Ahushh <ahushh@gmail.com>
2015-12-26 14:39:38 +0300

    Implement variables

--- main.cpp
+++ main.cpp
@@ -1,15 +1,22 @@
 #include <iostream>
 #include <string>
 #include <cmath>
+#include <ctype.h>
+#include <vector>
 
 using namespace std;
 
-const char quit = 'q';
-const char print = ';';
+const char quit = 'q'; // лексема выхода
+const char print = ';'; // лексема вычисления результата
+const char number = '0'; // лексема числа
+const char let = 'L'; // лексема let
+const char var_name = 'a'; // лексема name
 const string prompt = "> ";
 const string result = "= ";
+const string declare = "let";
 
 /* Exception class */
+/* Обертка над исключениями для удобства */
 class Exception {
 public:
     string err;
@@ -24,29 +31,35 @@ void error(string s) {
 }
 /* end of exception class */
 
-/* Token class class */
+/* Token class */
 class Token {
 public:
-    char kind;
+    char kind; /* вид лексемы */
     double value;
-
+    string name;
+    
     Token(char ch) : kind(ch), value(0){};
     Token(char ch, double val) : kind(ch), value(val){};
+    Token(char ch, string s) : kind(ch), name(s){};
 };
 
+/* Класс для считывания потока лексем */
 class Token_stream {
 public:
     Token_stream();
     Token get();
     void putback(Token t);
+    void ignore(char c);
 
 private:
     bool full;
     Token buffer;
 };
 
-Token_stream::Token_stream() : full(false), buffer(0) {}
+/* конструктор */
+Token_stream::Token_stream() : full(false), buffer(0) {} /* по-умолчанию в буфере нет объектов */
 
+/* возвращает лексему назад в поток */
 void Token_stream::putback(Token t) {
     if (full) {
         error("putback() buffer is full");
@@ -56,6 +69,7 @@ void Token_stream::putback(Token t) {
     full = true;
 }
 
+/* считываем символы и возвращает лексему */
 Token Token_stream::get() {
     if (full) {
         full = false;
@@ -65,24 +79,50 @@ Token Token_stream::get() {
     char ch;
     cin >> ch;
     switch (ch) {
-        case print:
-        case quit:
-        case 'l': case 's': case 'c': case '^':
-        case '{': case '}': case '(': case ')': case '+':
-        case '-': case '*': case '/': case '%': case '!':
-            return Token(ch);
+    case print:
+    case quit:
+    case '^':
+    case '{': case '}': case '(': case ')': case '+':
+    case '-': case '*': case '/': case '%': case '!':
+        return Token(ch);
 
-        case '.': case '0': case '1': case '2': case '3':
-        case '4': case '5': case '6': case '7': case '8': case '9': {
-            cin.putback(ch);
-            double val;
-            cin >> val;
-            return Token('8', val);
-        }
+    case '.': case '0': case '1': case '2': case '3':
+    case '4': case '5': case '6': case '7': case '8': case '9': {
+        cin.putback(ch); /* возвращаем цифру в стандартный поток для считывания целого числа через >> */
+        double val;
+        cin >> val;
+        return Token(number, val); /* number - вид лексемы, обознчающий число */
 
-        default:
-            error("bad token");
+    case '=':
+        return Token('=');
     }
+
+    default:
+        if (isalpha(ch)) { 
+            cin.putback(ch);
+            string s;
+            // считываем имя переменной
+            // содержит буквы, цифры, знак подчеркивания
+            while (cin.get(ch) && (isalpha(ch) || isdigit(ch) || ch == '_'))
+                s += ch;
+            cin.putback(ch);
+            if (s == declare) return Token(let); // ключевое слово let
+            return Token(var_name, s);
+        }
+        error("bad token");
+    }
+}
+
+/* отбрасывает символы, предшествующие символу c включительно */
+void Token_stream::ignore(char c) {
+    if (full && c == buffer.kind) { // сначала проверяем буфер
+        full = false;
+        return;
+    }
+    full = false;
+    char ch = 0;
+    while (cin >> c)
+        if (ch == c) return;
 }
 /* end of Token class */
 
@@ -95,14 +135,45 @@ public:
     Variable(string n, double v) : name(n), value(v) {}
 };
 
-// double get_value(string e)
-//{
-// for (int i = 0; i < var_table.size(); ++i)
-// if (var_table[i].name == s) return var_table[i].value;
-// error ("get: undefined variable");
-//}
+vector<Variable> var_table;
+
+/* возвращает значение переменной с именем s */
+double get_value(string s) {
+    for (int i = 0; i < var_table.size(); ++i) {
+        if (var_table[i].name == s) return var_table[i].value;
+    }
+    error("get: undefined variable "+s);
+}
+
+/* устанавливает значение d переменной s */
+void set_value(string s, double d) {
+    for (int i = 0; i < var_table.size(); ++i) {
+        if (var_table[i].name == s) {
+            var_table[i].value = d;
+            return;
+        }
+    }
+    error("set: undefined value "+s);
+}
+
+/* проверяет, определена ли переменная */
+bool is_declared(string var) {
+    for (int i = 0; i < var_table.size(); ++i) {
+        if (var_table[i].name == var) return true;
+    }
+    return false;
+}
+
+/* определяет переменную var со значением val */
+double define_name(string var, double val) {
+    if (is_declared(var))
+        error(var+" declared twice");
+    var_table.push_back( Variable(var, val) );
+    return val;
+}
 /* end of Variable class */
 
+/* функция факториала */
 double fact(double n) {
     double f = 1;
     for (int i = 1; i <= n; i++) {
@@ -113,161 +184,192 @@ double fact(double n) {
 }
 
 Token_stream ts;
+double expression(); // определено ниже
 
-double expression();
-
+/* первичное выражение */
 double primary() {
     Token t = ts.get();
     switch (t.kind) {
-        case '(': {
-            double d = expression();
-            t = ts.get();
-            if (t.kind != ')') error("')' expected");
-            return d;
-        }
+    case '(': {
+        double d = expression();
+        t = ts.get();
+        if (t.kind != ')') error("')' expected");
+        return d;
+    }
 
-        case '{': {
-            double d = expression();
-            t = ts.get();
-            if (t.kind != '}') error("'}' expected");
-            return d;
-        }
+    case '{': {
+        double d = expression();
+        t = ts.get();
+        if (t.kind != '}') error("'}' expected");
+        return d;
+    }
 
-        case '-':
-            return -primary();
+    case '-':
+        return -primary();
 
-        case '+':
-            return primary();
+    case '+':
+        return primary();
 
-        case 'l':
-            return log(primary());
-
-        case 's':
-            return sin(primary());
-
-        case 'c':
-            return cos(primary());
-
-        case '8':
-            return t.value;
-
-        default:
-            error("primary expected");
+    case var_name:
+        return get_value(t.name);
+    case number:
+        return t.value;
+        
+    default:
+        error("primary expected");
     }
 }
 
+/* постфиксное выражение */
 double postfix() {
     double left = primary();
     Token t = ts.get();
 
     while (true) {
         switch (t.kind) {
-            case '!':
-                left = fact(left);
-                t = ts.get();
-                break;
-            default:
-                ts.putback(t);
-                return left;
+        case '!':
+            left = fact(left);
+            t = ts.get();
+            break;
+        default:
+            ts.putback(t);
+            return left;
         }
     }
 }
 
+/* терм */
 double term() {
     double left = postfix();
     Token t = ts.get();
 
     while (true) {
         switch (t.kind) {
-            case '*':
-                left *= postfix();
-                t = ts.get();
-                break;
+        case '*':
+            left *= postfix();
+            t = ts.get();
+            break;
 
-            case '/': {
-                double d = postfix();
-                if (d == 0) {
-                    error("divide by zero");
-                }
-                left /= d;
-                t = ts.get();
-                break;
+        case '/': {
+            double d = postfix();
+            if (d == 0) {
+                error("divide by zero");
             }
+            left /= d;
+            t = ts.get();
+            break;
+        }
 
-            case '%': {
-                double d = term();
-                int i1 = int(left);
-                if (i1 != left) {
-                    error("left operand of % isn't an integer");
-                }
-                int i2 = int(d);
-                if (i2 != d) {
-                    error("right operand of % isn't an integer");
-                }
-                if (i2 == 0) {
-                    error("divide by zero");
-                }
-                left = i1 % i2;
-                // return left;
-                ts.putback(t);
-                break;
+        case '%': {
+            int a = int(left);
+            if (a != left) {
+                error("left operand of % isn't an integer");
             }
-
-            case '^': {
-                double d = postfix();
-                left = pow(left, d);
-                t = ts.get();
-                break;
+            double d = term();
+            int b = int(d);
+            if (b != d) {
+                error("right operand of % isn't an integer");
             }
+            if (b == 0) {
+                error("divide by zero");
+            }
+            left = a % b;
+            return left;
+            //ts.putback(t);
+            break;
+        }
 
-            default:
-                ts.putback(t);
-                return left;
+        case '^': {
+            double d = postfix();
+            left = pow(left, d);
+            t = ts.get();
+            break;
+        }
+
+        default:
+            ts.putback(t);
+            return left;
         }
     }
 }
 
+/* выражение */
 double expression() {
-    double left = term();
-    Token t = ts.get();
+    double left = term(); /* считываем и вычисляем терм */
+    Token t = ts.get(); /* считываем следующую лексему из потока */
 
     while (true) {
         switch (t.kind) {
-            case '+':
-                left += term();
-                t = ts.get();
-                break;
+        case '+':
+            left += term();
+            t = ts.get();
+            break;
 
-            case '-':
-                left -= term();
-                t = ts.get();
-                break;
+        case '-':
+            left -= term();
+            t = ts.get();
+            break;
 
-            default:
-                ts.putback(t);
-                return left;
+        default:
+            ts.putback(t);  /* возвращаем лексему обратно в поток */
+            return left; /* получившееся выражение - терм, возвращаем его */
         }
     }
 }
 
+/* объявление */
+double declaration() {
+    Token t = ts.get();
+    if (t.kind != var_name) error("expected let in variable declaration");
+
+    string var_name1 = t.name;
+
+    Token t2 = ts.get();
+    if (t2.kind != '=') error("missed symbol = in declaration of "+var_name1+" variable");
+
+    double d = expression();
+    define_name(var_name1, d);
+    return d;
+}
+
+/* инструкция */
+double statement() {
+    Token t = ts.get();
+    switch (t.kind) {
+    case let:
+        return declaration();
+    default:
+        ts.putback(t);
+        return expression();
+    }
+}
+
+/* вычисления */
+int calculate() {
+    cout << prompt;
+    Token t = ts.get();
+    while (t.kind == print) {
+        t = ts.get();
+    }
+    if (t.kind == quit) {
+        return 1;
+    }
+
+    ts.putback(t);
+    cout << result << statement() << endl;
+    return 0; // continue
+}
+
 int main(int argc, char* argv[]) {
+    define_name("pi",3.141592653);
+    define_name("e" ,2.718281828);
     while (cin) {
         try {
-            cout << prompt;
-            Token t = ts.get();
-            while (t.kind == print) {
-                t = ts.get();
-            }
-            if (t.kind == quit) {
-                return 0;
-            }
-
-            ts.putback(t);
-            cout << result << expression() << endl;
-
+            int exit = calculate();
+            if (exit) return 0;
         } catch (Exception& e) {
             cerr << e.what() << endl;
-
+            ts.ignore(print); // игнорируем поток символов с ошибкой до символа печати
         } catch (...) {
             return 2;
         }
@@ -276,7 +378,21 @@ int main(int argc, char* argv[]) {
     return 0;
 }
 
-/*
+/* Грамматика
+Вычисление:
+    Инструкция
+    Печать
+    Выход   
+    Инструкция вычисления
+Инструкция:
+    Объявление
+    Выражение
+Объявление:
+    "let" Имя "=" Выражение
+Печать:
+    ;
+Выход:
+    q
 Выражение:
     Терм
     Выражение "+" Терм
@@ -289,10 +405,11 @@ int main(int argc, char* argv[]) {
 Постфикс:
     Первичное выражение "!"
 Первичное выражение:
+    Имя
     Число
     "(" Выражение ")"
-    "-" Первичное выражение
-    "+" Первичное выражение
+    "-" Первичное выражение // унарный минус - отрицательное число
+    "+" Первичное выражение // унарный плю
 Число:
     литерал_с_плавающей_точкой
 
